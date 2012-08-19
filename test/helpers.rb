da2b881dbf694f875a3d1ed15c627922c09af942
Mon ouïe <mon.ouie@gmail.com>
2012-08-19 16:24:28 +0200

    Removed useless call to File.join in test helpers

--- test/helpers.rb
+++ test/helpers.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path(File.join("../lib"), File.dirname(__FILE__))
+$LOAD_PATH.unshift File.expand_path("../lib", File.dirname(__FILE__))
 
 require 'riot'
 require 'pry-coolline'
